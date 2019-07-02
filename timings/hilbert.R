70a1d5843772504692208918d12749524a39dc4b
chaudhur <chaudhur@enst.fr>
2019-07-02 14:06:54 +0200

    cleaning up

--- timings/hilbert.R
+++
@@ -1,3 +0,0 @@
-library( HilbertVisGUI )
-x<-read.table("./del")
-hilbertDisplay( (x[,2]-mean(x[,2]))/(sd(x[,2])))
--- timings/process.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-unzip timings.zip
-for i in `ls *.txt`; do ./parse.py $i 16777216 > $i.r; rm -rf $i;done
-paste -d, *.r > result
-rm -rf *.r
-
